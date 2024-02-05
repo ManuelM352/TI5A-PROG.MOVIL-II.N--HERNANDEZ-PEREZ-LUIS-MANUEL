7cc1e90cbe1addff409a666535145086d1ce20c7
Manuel <perezmane07@gmail.com>
2024-02-05 14:30:21 -0600

    Avance: Inserción de dependencias Creación y Actualización de clases para servir como dependencias.

--- basic-android-kotlin-compose-training-mars-photos-repo-starter/basic-android-kotlin-compose-training-mars-photos-repo-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ basic-android-kotlin-compose-training-mars-photos-repo-starter/basic-android-kotlin-compose-training-mars-photos-repo-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -22,30 +22,7 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.http.GET
-
-
-
-    /**
-     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
-     */
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BASE_URL)
-        .build()
-
-    /**
-     * Retrofit service object for creating api calls
-     */
     interface MarsApiService {
         @GET("photos")
         suspend fun getPhotos(): List<MarsPhoto>
     }
-
-    /**
-     * A public Api object that exposes the lazy-initialized Retrofit service
-     */
-    object MarsApi {
-        val retrofitService: MarsApiService by lazy {
-            retrofit.create(MarsApiService::class.java)
-        }
-}
