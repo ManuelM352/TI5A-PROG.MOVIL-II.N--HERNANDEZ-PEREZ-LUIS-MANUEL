39f132dc8f19b8200b5b206b5170c8dcbaa732ba
Manuel <perezmane07@gmail.com>
2024-02-23 18:53:08 -0600

    Avance: Restricciones de trabajos Se integra la funcionalidad para cuando el dispositivo tenga bateria baja la solicitud se posponga.

--- basic-android-kotlin-compose-training-workmanager-intermediate/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ basic-android-kotlin-compose-training-workmanager-intermediate/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,6 +19,7 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.asFlow
+import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
@@ -55,9 +56,15 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
         var continuation = workManager.beginUniqueWork(IMAGE_MANIPULATION_WORK_NAME, ExistingWorkPolicy.REPLACE,
             OneTimeWorkRequest.from(CleanupWorker::class.java))
 
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+
         // Add WorkRequest to blur the image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
-
+        
+        blurBuilder.setConstraints(constraints)
         // Input the Uri for the blur operation along with the blur level
         blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
 
